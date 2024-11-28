33ba8a38faa882c3540cd89e76bf2ea6002ad4f1
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-11-28 15:16:56 +0100

    changed cors config

--- src/main/java/org/example/greenpointbackend/security/SecurityConfig.java
+++ src/main/java/org/example/greenpointbackend/security/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Add your frontend URL
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Add your frontend URL
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
