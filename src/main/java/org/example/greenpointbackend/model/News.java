1f929d0f67700d1987325ebd154e6e30f52a22e3
hantamil <mira0004@stud.kea.dk>
2024-11-29 13:03:44 +0100

    added campaign, course and news classes to model

---
+++ src/main/java/org/example/greenpointbackend/model/News.java
@@ -0,0 +1,27 @@
+package org.example.greenpointbackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 2000)
+    private String description;
+
+    @Column(nullable = false)
+    private Date date;
+}
