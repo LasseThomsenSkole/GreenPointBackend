20b3c55861b067d2f1f481165c22d98924622a21
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-12-02 18:06:53 +0100

    removed comment

--- src/main/java/org/example/greenpointbackend/controller/AuthController.java
+++ src/main/java/org/example/greenpointbackend/controller/AuthController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//todo lav om til /auth/
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
