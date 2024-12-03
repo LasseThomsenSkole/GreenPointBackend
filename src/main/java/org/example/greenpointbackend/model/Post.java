9f72a865b51e5b6bb5549aa7b0efbbe1177a11fd
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-12-03 15:30:21 +0100

    added EnumType string to JobTitle

--- src/main/java/org/example/greenpointbackend/model/Post.java
+++ src/main/java/org/example/greenpointbackend/model/Post.java
@@ -26,6 +26,7 @@ public class Post {
     @Column(nullable = false)
     private LocalDate date;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private JobTitle jobTitle;
 }
