2ce51a47e59f56605a269cf0237cbf8ec9dc43c1
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-12-03 15:32:11 +0100

    changed string role to JobTitle

--- src/main/java/org/example/greenpointbackend/repository/CourseRepository.java
+++ src/main/java/org/example/greenpointbackend/repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package org.example.greenpointbackend.repository;
 
 import org.example.greenpointbackend.model.Course;
+import org.example.greenpointbackend.model.Enums.JobTitle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,6 @@ import java.util.List;
 
     @Repository
     public interface CourseRepository extends JpaRepository<Course, Long> {
-        List<Course> findCoursesByRole(String role);
+        List<Course> findCoursesByJobTitle(JobTitle jobTitle);
     }
 
