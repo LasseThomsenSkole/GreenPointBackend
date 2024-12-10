a97c7350f4c4cbb4f1ef5b08ea05746c76329f3a
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-12-10 11:57:20 +0100

    added jpa search method

--- src/main/java/org/example/greenpointbackend/repository/CourseRepository.java
+++ src/main/java/org/example/greenpointbackend/repository/CourseRepository.java
@@ -2,7 +2,12 @@ package org.example.greenpointbackend.repository;
 
 import org.example.greenpointbackend.model.Course;
 import org.example.greenpointbackend.model.Enums.JobTitle;
+import org.example.greenpointbackend.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +15,10 @@ import java.util.List;
     @Repository
     public interface CourseRepository extends JpaRepository<Course, Long> {
         List<Course> findCoursesByJobTitle(JobTitle jobTitle);
-    }
 
+        @Query("SELECT c FROM Course c WHERE " +
+                "(LOWER(c.title) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+                "OR LOWER(c.description) LIKE LOWER(CONCAT('%', :keyword, '%'))) ")
+        Page<Course> searchCourse(@Param("keyword") String keyword,
+                              Pageable pageable);
+    }
